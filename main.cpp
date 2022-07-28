b0be109fafdc35efcb9385d91ad9753d29e2962c
Reinaldo Felipe Hokari Soares Araujo <reinaldo.araujo@mercadolivre.com>
2022-07-28 14:52:07 -0400

    new_algorithm, sort by irf, save in file

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 #include <set>
 #include <vector>
 #include <math.h>
+
 #define lli long long int
-#define MAP_TYPE map <lli, vector< vector<Transaction> >  >
+#define MAP_TYPE2 map <lli, vector< vector<Transaction> >  >
+#define MAP_TYPE1 map <lli, map<int, vector <vector <Transaction> > > >
 using namespace std;
 
 class Transaction{
@@ -35,10 +37,18 @@ class Transaction{
 class Solution {
     public:
     vector<Transaction> S;
+    lli sum_irf;
+    lli sum_total;
 
     Solution(vector<Transaction> S){
-        sort(S.begin(), S.end());
         this->S = S;
+        this->sum_irf = 0;
+        this->sum_total = 0;
+        for(int i=0; i < S.size(); i++){
+            sum_irf += S[i].irf;
+            sum_total += S[i].irf;
+        }
+        sort(this->S.begin(), this->S.end());
     }
 
     int size(){
@@ -46,11 +56,17 @@ class Solution {
     }
 
     bool operator == (const Solution &b){
-        if(this->S.size() != b.S.size()) return false;
-        for(int i=0; i < b.S.size(); i++){
-            if(S[i] != b.S[i]) return false;
+        string a_id = "";
+        string b_id = "";
+
+        for(int i=0; i < this->S.size(); i++){
+            a_id += this->S[i].id;
         }
-        return true;
+
+        for(int i=0; i < b.S.size(); i++){
+            b_id += this->S[i].id;
+        }
+        return a_id == b_id;
     }
 
     bool operator < (const Solution &b) const {
@@ -62,42 +78,30 @@ class Solution {
         }
 
         for(int i=0; i < b.S.size(); i++){
-            b_id += this->S[i].id;
+            b_id += b.S[i].id;
         }
         return a_id < b_id;
     }
 
-    void printS() const{
-        cout << "(";
+    void printS(ostream &cout) const{
+        cout << "\"(";
         for(int i=0; i < this->S.size(); i++){
             if(i!=0) cout << ",";
             cout << this->S[i].id;
         }
-        cout << ")" << endl;
+        cout << ")\"";
     }
 };
 
+// Variables and constants
+
 const int decimal_fix = 100;
-
+lli irfTargetT = 0;
 vector <Transaction> transactions;
-
 string inputFile = "", outputFile = "";
-
-vector <vector <Transaction> > solutions;
-MAP_TYPE mapa;
-
-void printTransactions(){
-    cout << "=== PRINT TRANSACTIONS==" << endl;
-    for(int i=0; i < transactions.size();i++){
-        cout << "id: " << transactions[i].id << endl;
-        cout << "value: "  << transactions[i].value << endl;
-        cout << "irf: " << transactions[i].irf << endl;
-        // printf("value: %lf\n", float(transactions[i].value)/float(decimal_fix));
-        // printf("irf: %lf\n", float(transactions[i].irf)/float(decimal_fix));
-    }
-    cout << "=== END TRANSACTIONS==" << endl;
-}
-
+vector <Solution> solutions;
+MAP_TYPE2 mapa_alg2;
+MAP_TYPE1 mapa_alg1;
 
 void getInput(){
     ifstream fin;
@@ -123,30 +127,34 @@ void getInput(){
 
         transactions.push_back(Transaction(id, lli_value, lli_irf));
     }
+    fin.close();
 }
 
+bool sortbyirf(Solution a, Solution b){
+    double diff_a = abs(a.sum_irf - irfTargetT);
+    double diff_b = abs(b.sum_irf - irfTargetT);
+    return diff_a < diff_b;
+}
 
-void printSolutions(vector< vector<Transaction> > S){
-    lli sum_total = 0;
-    lli irf_total = 0;
-    if(S.empty()){
-        cout << "There is no solution." << endl;
+void outputSolutions(vector<Solution> solutions){
+    sort(solutions.begin(), solutions.end(), sortbyirf);
+
+    ofstream myfile;
+    myfile.open(outputFile);
+
+    if(solutions.empty()){
+        myfile << "There is no solution." << endl;
         return ;
     }
-    cout << "\"IDs\", \"SUM_TOTAL_AMOUNT\", \"SUM_IRF_AMOUNT\"" << endl;
+    myfile << "\"IDs\", \"SUM_TOTAL_AMOUNT\", \"SUM_IRF_AMOUNT\"" << endl;
 
-    for(int i=0; i < S.size(); i++){
-        cout << "\"(";
-        for(int j=0; j < S[i].size(); j++){
-            if(j!=0) cout << ",";
-            cout << S[i][j].id;
-            sum_total += S[i][j].value;
-            irf_total += S[i][j].irf;
-        }
-        cout << ")\",";
-        cout << "\"" << float(sum_total)/decimal_fix << "\",";
-        cout << "\"" << float(irf_total)/decimal_fix << "\"" << endl;
+    for(int i=0; i < solutions.size(); i++){
+        solutions[i].printS(myfile);
+        myfile << "\"" << float(solutions[i].sum_total)/decimal_fix << "\",";
+        myfile << "\"" << float(solutions[i].sum_irf)/decimal_fix << "\"" << endl;
     }
+
+    myfile.close();
 }
 
 void printStack(vector <Transaction> S){
@@ -166,51 +174,107 @@ void printStack(vector <Transaction> S){
     cout << " IRF_ALL: " << float(irf_total)/decimal_fix << endl;
 }
 
+lli min(lli a, lli b){
+    return a < b ? a : b;
+}
+
+lli getMin(){
+    lli minValue = INT_MAX;
+    for(int i=0; i < transactions.size(); i++){
+        minValue = min(minValue, transactions[i].value);
+    }
+    return minValue;
+}
+
 void algorithm2( lli SUM_TOTAL_AMOUNT, int totalElements)
 {
-    for(int i=1; i <= SUM_TOTAL_AMOUNT; i++){
+    int i = int(getMin());
+    for(;i <= SUM_TOTAL_AMOUNT; i++){
         for(int j=0; j < transactions.size(); j++){
             lli dif = i - transactions[j].value;
 
             if(dif < 0) continue;
-            //  Element found
-            if(dif == 0){
+            
+            if(dif == 0){ //  Element found
                 vector <Transaction> new_solution;
                 new_solution.push_back(transactions[j]);
-                mapa[i].push_back(new_solution);
+                mapa_alg2[i].push_back(new_solution);
             }
-        
-            // Finding another solution
-            if(not mapa[dif].empty()){
-                for(int k=0; k < mapa[dif].size(); k++){
-                    if(mapa[dif][k].size() < totalElements){
-                        vector <Transaction> new_solution = vector<Transaction>(mapa[dif][k].begin(), mapa[dif][k].end());
-                        new_solution.push_back(transactions[j]);
-                        mapa[i].push_back(new_solution);
+            else{
+                if(not mapa_alg2[dif].empty()){ // Finding another solution
+                    for(int k=0; k < mapa_alg2[dif].size(); k++){
+                        if(mapa_alg2[dif][k].size() <= totalElements){
+                            vector <Transaction> new_solution = mapa_alg2[dif][k];
+                            new_solution.push_back(transactions[j]);
+                            mapa_alg2[i].push_back(new_solution);
+                        }
                     }
                 }
             }
         }
     }
 }
+void algorithm1(int totalElements, lli value, vector<Transaction> conjunto, vector<Transaction> S)
+{
+    // Critério de parada
+    if(totalElements < 1 || value <= 0 ||
+       conjunto.empty() || totalElements > conjunto.size()
+    ){
+        return;
+    }
 
-vector< vector <Transaction> > getSolutions(lli amountTarget, int nElements){
-    vector <vector <Transaction> > output;
-    set <Solution> Ss;
+    int i = conjunto.size() - 1;
+    conjunto.pop_back();
 
-    if(mapa.find(amountTarget) != mapa.end()){
-        for(int i=0; i < mapa[amountTarget].size();i++){
-            vector<Transaction> current = mapa[amountTarget][i];
+    S.push_back(conjunto[i]);
+
+    Transaction tCurrent = conjunto[i];
+    lli valueCurrent = conjunto[i].value;
+    string idCurrent = conjunto[i].id;
+
+    lli new_value = value - valueCurrent;
+
+    if(new_value == 0){
+        vector <Transaction> newSolution2;
+        newSolution2.push_back(tCurrent);
+        mapa_alg1[value][totalElements].push_back(newSolution2);
+        if(totalElements == 1){
+            printStack(S);
+            solutions.push_back(Solution(S));
+        }
+    }
+    else if(new_value > 0){
+        if(mapa_alg1[new_value][totalElements-1].empty()){
+            algorithm1(totalElements-1, new_value,conjunto, S);
+        }
+
+        for(int i=0; i < mapa_alg1[new_value][totalElements-1].size();i++){
+            vector<Transaction> new_solution = mapa_alg1[new_value][totalElements-1][i];
+            new_solution.push_back(tCurrent);
+            mapa_alg1[value][totalElements].push_back(new_solution);
+        }
+        S.pop_back();
+        algorithm1(totalElements, value,conjunto, S);
+    }
+}
+
+vector <Solution> getSolutions(lli amountTarget, int nElements){
+    vector <Solution> output;
+    set <Solution> S;
+
+    if(mapa_alg2.find(amountTarget) != mapa_alg2.end()){
+        for(int i=0; i < mapa_alg2[amountTarget].size();i++){
+            vector<Transaction> current = mapa_alg2[amountTarget][i];
             if(current.size() == nElements){
-                Ss.insert(Solution(current));
+                S.insert(Solution(current));
             }
         }
     }
 
-    set <Solution> :: iterator it = Ss.begin();
+    set <Solution> :: iterator it = S.begin();
 
-    for(;it != Ss.end(); it++){
-        output.push_back(it->S);
+    for(;it != S.end(); it++){
+        output.push_back(*it);
     }
 
     return output;
@@ -220,13 +284,14 @@ int main(int argc, char *argv[]){
     inputFile = string(argv[1]);
     outputFile = string(argv[2]);
     string amountTargetS = string(argv[3]);
-    string nElementsS = string(argv[4]);
     string irfTargetS = string(argv[4]);
+    string nElementsS = string(argv[5]);
+    string algorithm = string(argv[6]);
     double amountTarget = 0.0;
     double irfTarget = 0.0;
     int nElements = 0;
     
-    if(argc != 5){
+    if(argc != 7){
         cout << "Error: Provide input, output, key value and quantity of elements." << endl;
         cout << argc << " arguments." << endl;
         return -1;
@@ -241,13 +306,29 @@ int main(int argc, char *argv[]){
         cout << "A float and integer are expected." << endl;  
     }
 
+    // inputFile = "input2.csv";
+    // outputFile = "output3.csv";
+    // string algorithm = "algorithm1";
+    // double amountTarget = 96.8;
+    // double irfTarget = 0.9;
+    // int nElements = 3;
+    
     getInput();
 
     vector <Transaction> empty;
     lli amountTargetT = trunc(amountTarget*decimal_fix);
+    irfTargetT = trunc(irfTarget*decimal_fix);
  
-    generateCombinations(amountTargetT, nElements);
-    printSolutions(getSolutions(amountTargetT, nElements));
+    if(algorithm == "algorithm1"){
+        vector <Transaction> empty;
+        algorithm1(nElements, amountTargetT, transactions, empty);
+        outputSolutions(solutions);
+    }
+
+    if(algorithm == "algorithm2") {
+        algorithm2(amountTargetT, nElements);
+        outputSolutions(getSolutions(amountTargetT, nElements));
+    }
 
     return 0;
 }
--- start.sh
+++ start.sh
@@ -8,10 +8,21 @@ read inputFile
 echo "Output file name (Ex: output.csv): "
 read outputFile
 echo "Amount target to be found: "
-read keyValue
+read targetValue
+echo "IRF target to be found: "
+read targetIRF
 echo "Number of elements for combination: "
 read nElements
+echo "Algorithm 1 or 2"
+read algorithm
 
-echo "Running..."
-./bin $inputFile $outputFile $keyValue $nElements > $outputFile
+if [ $algorithm = 2 ]
+then
+echo "Running Algorithm 2"
+./bin $inputFile $outputFile $targetValue $targetIRF $nElements algorithm2
+else
+echo "Running Algorithm 1"
+./bin $inputFile $outputFile $targetValue $targetIRF $nElements algorithm1
+fi
+
 echo "Finished execution file were created."
